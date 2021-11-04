aea500c0acb4681db9d931fb7c9341474f0aadee
ErickMoreira13 <marianoerick345@gmai.com>
2021-11-03 22:45:10 -0300

    Refatoração das rotas

--- src/Pages/Personagens/index.js
+++ src/Pages/Personagens/index.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from 'react';
+import HeaderResponsive from '../../Components/HeaderResponsive';
+import api from '../../Api';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 function Personagens(){
+    const [data, setData] = useState(null);
+
+    useEffect( () => {
+        async function loadAll(){
+            let res = await api.getPeronagemByName();
+            setData(res.data)
+            console.log(res.data)
+        }
+        loadAll();
+    }, [data]);
+
+    
+    
     return(
         <div>
-            Hello world
+            <HeaderResponsive />
+            <div className = "search-container">
+                <form>
+                    <input />
+                    <FontAwesomeIcon icon={faSearch} size = "lg" />
+                </form>
+            </div>
         </div>
     );
 }
