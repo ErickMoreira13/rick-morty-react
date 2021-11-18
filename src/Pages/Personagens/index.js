3284e0b10c2d84f61f7d525c3b32b7d1ddea5bfa
ErickMoreira13 <marianoerick345@gmai.com>
2021-11-17 23:33:19 -0300

    Atualizando pagina de busca de personagens

--- src/Pages/Personagens/index.js
+++ src/Pages/Personagens/index.js
@@ -1,31 +1,57 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import HeaderResponsive from '../../Components/HeaderResponsive';
 import api from '../../Api';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import './styles.css';
+import Card from '../../Components/Card';
 
 function Personagens(){
     const [data, setData] = useState(null);
+    const [name, setName] = useState("");
+    
+    async function load(){
+        let res = await api.getPeronagemByName(name);
+        setData(res)
+    }
 
-    useEffect( () => {
-        async function loadAll(){
-            let res = await api.getPeronagemByName();
-            setData(res.data)
-            console.log(res.data)
+    async function searchCharacter(event){
+        event.preventDefault();
+        setData(null);
+        if(name){
+            load();
         }
-        loadAll();
-    }, [data]);
+    }
+ 
+    function handleName(event){
+        setName(event.target.value);
+    }
 
-    
-    
     return(
         <div>
             <HeaderResponsive />
             <div className = "search-container">
-                <form>
-                    <input />
-                    <FontAwesomeIcon icon={faSearch} size = "lg" />
+                <form onSubmit={searchCharacter}>
+                    <input onChange={handleName}/>
+                    <button type="submit">
+                        <FontAwesomeIcon icon={faSearch} size = "lg" />
+                    </button>
                 </form>
+            </div>
+            <div className="results-container">
+                {data &&
+                    ((data.error) 
+                        ? <h1>Busca não encontrada</h1> 
+                        : data.results.map((item, key) => {
+                            return <Card 
+                                key={item.key}
+                                id={item.id} 
+                                img={item.image} 
+                                name={item.name} 
+                            />
+                        })
+                    )
+                }
             </div>
         </div>
     );
