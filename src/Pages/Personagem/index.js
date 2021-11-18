c3621d56fc8c0577f2b4fc839b51154d0161a03e
ErickMoreira13 <marianoerick345@gmai.com>
2021-11-17 23:36:07 -0300

    Adicionando pagina do personagem

--- src/Pages/Personagem/index.js
+++ src/Pages/Personagem/index.js
@@ -1,7 +1,64 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../Api";
+import HeaderResponsive from "../../Components/HeaderResponsive";
+import "./styles.css";
+
 function Personagem(){
+    const { id } = useParams();
+    const [data, setData] = useState(null);
+    const [dateCreated, setDateCreated] = useState(null);
+
+    useEffect( () =>{
+        const load = async()=>{
+            let response = await api.getPeronagemById(id);
+            setData(response);
+
+            let data_criacao = new Date(data.created);
+            data_criacao = data_criacao.toLocaleDateString("pt-Br");
+
+            setDateCreated(data_criacao);
+        }
+        load();
+    }, [id, data.created]);
+
     return(
-        <div>
-            Hello personagem
+        <div className="personagem-container">
+            { data && 
+                (
+                    <>
+                        <HeaderResponsive/>
+                        <div className="full-info-data-container">
+                            <img src={data.image} alt={data.name}/>
+                            <div className="info-data-container">
+                                <h1>Informações Completas</h1>
+                                <p>Nome:</p>
+                                <span>{data.name}</span>
+
+                                <p>Estado:</p>
+                                <span>{data.status}</span>
+
+                                <p>Espécie:</p>
+                                <span>{data.species}</span>
+
+                                <p>Gênero:</p>
+                                <span>{data.gender}</span>
+
+                                <p>Visto pela primeira vez:</p>
+                                <span>{data.origin.name}</span>
+
+                                <p>Visto pela ultima vez:</p>
+                                <span>{data.location.name}</span>
+
+                                <p>Criado em:</p>
+                                <span>{dateCreated}</span>
+                            </div>
+                        </div>
+                    </>
+                    
+                )
+            }
+            
         </div>
     );
 }
